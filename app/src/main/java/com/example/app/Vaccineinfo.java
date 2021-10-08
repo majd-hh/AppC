7b554d0946b858eee86e77298d0776b935fb8ed5
majd-hh <majdhussin@hotmail.com>
2021-10-08 22:23:51 +0200

    cov app

--- app/src/main/java/com/example/app/Vaccineinfo.java
+++ app/src/main/java/com/example/app/Vaccineinfo.java
@@ -6,137 +6,190 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Vaccineinfo {
 
+
     String url="https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/json/";
     Context context;
     public Vaccineinfo(Context context) {
-            this.context = context;
+        this.context = context;
+    }
+
+    public interface VolleyResponseListener{
+        void onError(String message);
+        void onResponse(List<VaccineData> VaccineData);
+
+    }
+
+    public void getVaccineData(String country,String Age,String vaccineType,com.example.app.Vaccineinfo.VolleyResponseListener volleyResponseListener){
+
+        String countryName="",AGE="",Vaccine="";
+
+
+        if (country.isEmpty()){
+            countryName="SE";
+            // System.out.println("\n \n \n"+"countryName.isEmpty()"+"\n \n \n");
+        }
+        else {
+            countryName=country;
         }
 
-        public interface VolleyResponseListener{
-            void onError(String message);
-            void onResponse(List<VaccineData> VaccineData);
-
+        if (Age.isEmpty()){
+            AGE="ALL";
+        }
+        else {
+            AGE=Age;
+        }
+        if (vaccineType.isEmpty()){
+            Vaccine="ALL";
+        }
+        else {
+            Vaccine=vaccineType;
         }
 
-        public void getVaccineData(String country,String Age,String vaccineType,com.example.app.Vaccineinfo.VolleyResponseListener volleyResponseListener){
-
-            String countryName="",AGE="",Vaccine="";
 
 
-            if (country.isEmpty()){
-                countryName="SE";
-                System.out.println("\n \n \n"+"countryName.isEmpty()"+"\n \n \n");
-            }
-            else {
-                countryName=country;
-            }
+        String finalCountryName = countryName;
+        String finalAGE = AGE;
+        String finalVaccine = Vaccine;
 
-            if (Age.isEmpty()){
-                AGE="ALL";
-            }
-            else {
-                AGE=Age;
-            }
-            if (vaccineType.isEmpty()){
-                Vaccine="ALL";
-            }
-            else {
-                Vaccine=vaccineType;
-            }
+        System.out.println("\n \n "+ "*****Country:::  "+countryName+"  AGE:: " +AGE+"  Vaccine:: " + Vaccine);
+        //get json object
+        List<VaccineData> ViccenReport = new ArrayList<>();
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET,url,null ,new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray ViccenData= response.getJSONArray("records");
+                    System.out.println("\n \n "+ "*********** VaccineData Array i info:::  " +ViccenData.length()+"************\n \n  ");
+                    System.out.println("\n \n "+ "*****FCountry:::  "+finalCountryName+"  finalAGE:: " +finalAGE+"  finalVaccine:: " + finalVaccine);
+                    //get data
+                    for (int i=0;i<ViccenData.length();i++){
+                        VaccineData getdata = new VaccineData();
+                        JSONObject Vaccineinfo = (JSONObject) ViccenData.get(i);
 
-
-
-            String finalCountryName = countryName;
-            String finalAGE = AGE;
-            String finalVaccine = Vaccine;
-
-            System.out.println("\n \n "+ "*****Country:::  "+countryName+"  AGE:: " +AGE+"  Vaccine:: " + Vaccine);
-            //get json object
-            List<VaccineData> ViccenReport = new ArrayList<>();
-            JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET,url,null ,new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    try {
-                        JSONArray ViccenData= response.getJSONArray("records");
-                        System.out.println("\n \n "+ "*********** VaccineData Array i info:::  " +ViccenData.length()+"************\n \n  ");
-                        System.out.println("\n \n "+ "*****FCountry:::  "+finalCountryName+"  finalAGE:: " +finalAGE+"  finalVaccine:: " + finalVaccine);
-                        //get data
-                        for (int i=0;i<ViccenData.length();i++){
-                            VaccineData getdata = new VaccineData();
-                            JSONObject Vaccineinfo = (JSONObject) ViccenData.get(i);
-
-                            if ( ( Vaccineinfo.getString("ReportingCountry").equals(finalCountryName) ) ) {
-                                    if (  (Vaccineinfo.getString("TargetGroup").equals(finalAGE))) {
-                                        if (finalVaccine.equals("ALL")){
-                                            getdata.setYearWeekISO(Vaccineinfo.getString("YearWeekISO"));
-                                            getdata.setFirstDose(Vaccineinfo.getInt("FirstDose"));
-                                            getdata.setFirstDoseRefused("");
-                                            getdata.setSecondDose(Vaccineinfo.getInt("SecondDose"));
-                                            getdata.setUnknownDose(0);
-                                            getdata.setNumberDosesReceived(Vaccineinfo.getString("NumberDosesReceived"));
-                                            if (getdata.getNumberDosesReceived().equals("")) {
-                                                getdata.setNumberDosesReceived("0");
-                                            }
-                                            getdata.setRegion("");
-                                            getdata.setPopulation(Vaccineinfo.getInt("Population"));
-                                            getdata.setReportingCountry(Vaccineinfo.getString("ReportingCountry"));
-                                            getdata.setTargetGroup(Vaccineinfo.getString("TargetGroup"));
-                                            getdata.setVaccine(Vaccineinfo.getString("Vaccine"));
-                                            getdata.setDenominator(Vaccineinfo.getString("Denominator"));
-                                            ViccenReport.add(getdata);
-                                        }else {
-                                            if ( (Vaccineinfo.getString("Vaccine").equals(finalVaccine)) ) {
-                                                getdata.setYearWeekISO(Vaccineinfo.getString("YearWeekISO"));
-                                                getdata.setFirstDose(Vaccineinfo.getInt("FirstDose"));
-                                                getdata.setFirstDoseRefused("");
-                                                getdata.setSecondDose(Vaccineinfo.getInt("SecondDose"));
-                                                getdata.setUnknownDose(0);
-                                                getdata.setNumberDosesReceived(Vaccineinfo.getString("NumberDosesReceived"));
-                                                if (getdata.getNumberDosesReceived().equals("")) {
-                                                    getdata.setNumberDosesReceived("0");
-                                                }
-                                                getdata.setRegion("");
-                                                getdata.setPopulation(Vaccineinfo.getInt("Population"));
-                                                getdata.setReportingCountry(Vaccineinfo.getString("ReportingCountry"));
-                                                getdata.setTargetGroup(Vaccineinfo.getString("TargetGroup"));
-                                                getdata.setVaccine(Vaccineinfo.getString("Vaccine"));
-                                                getdata.setDenominator(Vaccineinfo.getString("Denominator"));
-                                                ViccenReport.add(getdata);
-                                            }
-
-                                        }
+                        if ( ( Vaccineinfo.getString("ReportingCountry").equals(finalCountryName) ) ) {
+                            if (  (Vaccineinfo.getString("TargetGroup").equals(finalAGE))) {
+                                if (finalVaccine.equals("ALL")){
+                                    System.out.println("in ALL\n");
+                                    getdata.setYearWeekISO(Vaccineinfo.getString("YearWeekISO"));
+                                    getdata.setFirstDose(Vaccineinfo.getInt("FirstDose"));
+                                    getdata.setFirstDoseRefused("");
+                                    getdata.setSecondDose(Vaccineinfo.getInt("SecondDose"));
+                                    getdata.setUnknownDose(0);
+                                    getdata.setNumberDosesReceived(Vaccineinfo.getString("NumberDosesReceived"));
+                                    if (getdata.getNumberDosesReceived().equals("")) {
+                                        getdata.setNumberDosesReceived("0");
                                     }
+                                    getdata.setRegion("");
+                                    getdata.setPopulation(Vaccineinfo.getInt("Population"));
+                                    getdata.setReportingCountry(Vaccineinfo.getString("ReportingCountry"));
+                                    getdata.setTargetGroup(Vaccineinfo.getString("TargetGroup"));
+                                    getdata.setVaccine(Vaccineinfo.getString("Vaccine"));
+                                    getdata.setDenominator(Vaccineinfo.getString("Denominator"));
+                                    ViccenReport.add(getdata);
+                                }else {
+                                    if ( (Vaccineinfo.getString("Vaccine").equals(finalVaccine))&&
+                                            ( Vaccineinfo.getString("ReportingCountry").equals(finalCountryName) )&&
+                                            (Vaccineinfo.getString("TargetGroup").equals(finalAGE))) {
+                                        System.out.println("else\n");
 
+                                        getdata.setYearWeekISO(Vaccineinfo.getString("YearWeekISO"));
+                                        getdata.setFirstDose(Vaccineinfo.getInt("FirstDose"));
+                                        getdata.setFirstDoseRefused("");
+                                        getdata.setSecondDose(Vaccineinfo.getInt("SecondDose"));
+                                        getdata.setUnknownDose(0);
+                                        getdata.setNumberDosesReceived(Vaccineinfo.getString("NumberDosesReceived"));
+                                        if (getdata.getNumberDosesReceived().equals("")) {
+                                            getdata.setNumberDosesReceived("0");
+                                        }
+                                        getdata.setRegion("");
+                                        getdata.setPopulation(Vaccineinfo.getInt("Population"));
+                                        getdata.setReportingCountry(Vaccineinfo.getString("ReportingCountry"));
+                                        getdata.setTargetGroup(Vaccineinfo.getString("TargetGroup"));
+                                        getdata.setVaccine(Vaccineinfo.getString("Vaccine"));
+                                        getdata.setDenominator(Vaccineinfo.getString("Denominator"));
+                                        ViccenReport.add(getdata);
+                                    }if (finalVaccine.equals("COM")){
+                                        System.out.println("in com\n");
+                                        getdata.setYearWeekISO(Vaccineinfo.getString("YearWeekISO"));
+                                        getdata.setFirstDose(Vaccineinfo.getInt("FirstDose"));
+                                        getdata.setFirstDoseRefused("");
+                                        getdata.setSecondDose(Vaccineinfo.getInt("SecondDose"));
+                                        getdata.setUnknownDose(0);
+                                        getdata.setNumberDosesReceived(Vaccineinfo.getString("NumberDosesReceived"));
+                                        if (getdata.getNumberDosesReceived().equals("")) {
+                                            getdata.setNumberDosesReceived("0");
+                                        }
+                                        getdata.setRegion("");
+                                        getdata.setPopulation(Vaccineinfo.getInt("Population"));
+                                        getdata.setReportingCountry(Vaccineinfo.getString("ReportingCountry"));
+                                        getdata.setTargetGroup(Vaccineinfo.getString("TargetGroup"));
+                                        getdata.setVaccine(Vaccineinfo.getString("Vaccine"));
+                                        getdata.setDenominator(Vaccineinfo.getString("Denominator"));
+                                        ViccenReport.add(getdata);}
+                                    if (finalVaccine.equals("MOD")){
+                                        System.out.println("in mod\n");
+                                        getdata.setYearWeekISO(Vaccineinfo.getString("YearWeekISO"));
+                                        getdata.setFirstDose(Vaccineinfo.getInt("FirstDose"));
+                                        getdata.setFirstDoseRefused("");
+                                        getdata.setSecondDose(Vaccineinfo.getInt("SecondDose"));
+                                        getdata.setUnknownDose(0);
+                                        getdata.setNumberDosesReceived(Vaccineinfo.getString("NumberDosesReceived"));
+                                        if (getdata.getNumberDosesReceived().equals("")) {
+                                            getdata.setNumberDosesReceived("0");
+                                        }
+                                        getdata.setRegion("");
+                                        getdata.setPopulation(Vaccineinfo.getInt("Population"));
+                                        getdata.setReportingCountry(Vaccineinfo.getString("ReportingCountry"));
+                                        getdata.setTargetGroup(Vaccineinfo.getString("TargetGroup"));
+                                        getdata.setVaccine(Vaccineinfo.getString("Vaccine"));
+                                        getdata.setDenominator(Vaccineinfo.getString("Denominator"));
+                                        ViccenReport.add(getdata);}if (finalVaccine.equals("AZ")){
+                                        System.out.println("in AZ\n");
+                                        getdata.setYearWeekISO(Vaccineinfo.getString("YearWeekISO"));
+                                        getdata.setFirstDose(Vaccineinfo.getInt("FirstDose"));
+                                        getdata.setFirstDoseRefused("");
+                                        getdata.setSecondDose(Vaccineinfo.getInt("SecondDose"));
+                                        getdata.setUnknownDose(0);
+                                        getdata.setNumberDosesReceived(Vaccineinfo.getString("NumberDosesReceived"));
+                                        if (getdata.getNumberDosesReceived().equals("")) {
+                                            getdata.setNumberDosesReceived("0");
+                                        }
+                                        getdata.setRegion("");
+                                        getdata.setPopulation(Vaccineinfo.getInt("Population"));
+                                        getdata.setReportingCountry(Vaccineinfo.getString("ReportingCountry"));
+                                        getdata.setTargetGroup(Vaccineinfo.getString("TargetGroup"));
+                                        getdata.setVaccine(Vaccineinfo.getString("Vaccine"));
+                                        getdata.setDenominator(Vaccineinfo.getString("Denominator"));
+                                        ViccenReport.add(getdata);}
+                                }
                             }
 
                         }
-                        volleyResponseListener.onResponse(ViccenReport);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+
                     }
+                    volleyResponseListener.onResponse(ViccenReport);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            },new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    volleyResponseListener.onError("something wrong");
-                }
-            });
-            MySingleton.getInstance(context).addToRequestQueue(request);
-
-        }
-
-
+            }
+        },new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                volleyResponseListener.onError("something wrong");
+            }
+        });
+        MySingleton.getInstance(context).addToRequestQueue(request);
 
     }
+
+}
 
 
